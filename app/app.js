78390d8b4d608655f31c44b190d769fbbab35ce1
William Guo <yueguo@ebay.com>
2016-09-09 03:14:47 -0700

    init

---
+++ app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'ngResource', 
+  'ui.router', 
+  'ui.bootstrap', 
+  'highcharts-ng',
+  'myApp.version'
+]).config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+
+  // $locationProvider.html5Mode(true);
+
+});
